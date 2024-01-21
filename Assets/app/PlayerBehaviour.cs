48fcd6f6352cd7a6c95cfd93bd7c2ba6f53019a1
Valentin <v.sourice@keyveo.com>
2024-01-21 09:05:33 +0100

    a

--- Assets/app/PlayerBehaviour.cs
+++ Assets/app/PlayerBehaviour.cs
@@ -4,33 +4,15 @@ using UnityEngine.InputSystem;
 public class PlayerBehaviour : MonoBehaviour
 {
     [SerializeField] private InputActionReference move;
-    [SerializeField] private InputActionReference attack;
     [SerializeField] private Rigidbody rb;
-    [SerializeField] private float forwardSpeed = 10;
-    [SerializeField] private float backwardSpeed = 2;
-    [SerializeField] private float turnSpeed = 5;
-
-    void OnEnable()
-    {
-        attack.action.performed += ctx => Attack();
-    }
-
-    void OnDisable()
-    {
-        attack.action.performed -= ctx => Attack();
-    }
+    [SerializeField] private float speed;
+    [SerializeField] private float turnSpeed;
 
     void FixedUpdate()
     {
-        // TODO improve movement system
         var moveInput = move.action.ReadValue<Vector2>();
-        rb.AddTorque(new Vector3(0, moveInput.x, 0) * turnSpeed);
-        rb.AddForce(forwardSpeed * moveInput.y * transform.forward);
-    }
-
-    private void Attack()
-    {
-        // TODO
-        Debug.Log("Attack");
+        rb.AddForce(speed * moveInput.y * transform.forward);
+        var forwardVelocity = Vector3.Dot(rb.velocity, transform.forward);
+        rb.AddTorque(forwardVelocity * moveInput.x * turnSpeed * transform.up);
     }
 }
